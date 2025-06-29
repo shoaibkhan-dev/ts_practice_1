6820d74a0818be1c7310709c9583ea12fe13d0eb
Shoaib khan <shoaibmangal@yahoo.com>
2025-06-29 23:58:40 +0500

    add work

--- index.ts
+++ index.ts
@@ -1,2 +1,55 @@
+// interface Person<T, U> {
+//   fullName: T;
+//   age: U;
+// }
 
-let age: number = 20;
+// const person1: Person <string, number> = {
+//     fullName: "shoaib",
+//     age: 23
+// }
+
+// const person2: Person<{ first: string; last: string }, string> = {
+//   fullName: { first: "John", last: "Doe" },
+//   age: "25"
+// };
+
+
+// function processWithFilter<T extends { filter: Function }>(arg: T): T {
+//   arg.filter((item: any) => true); 
+//   return arg;
+// }
+
+
+// const numbers = [1, 2, 3, 4];
+// const result = processWithFilter(numbers);
+
+// console.log(result);
+
+
+// enum Diraction {
+//     up,
+//     down,
+//     left,
+//     right,
+// }
+
+// console.log(Diraction.up)
+// console.log(Diraction.down)
+
+enum HumanEnum{
+   M = "MALE",
+   F = "FEMALE",
+
+}
+
+interface Human {
+    gender : HumanEnum;
+}
+
+const person: Human = {
+  gender: HumanEnum.M
+};
+
+console.log(person.gender); // Output: "MALE"
+
+const person1: string = "shoaib"
