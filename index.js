2098793d6f2e30037a65fe530e5c8916281fc4fb
Shoaib khan <shoaibmangal@yahoo.com>
2025-07-01 23:50:07 +0500

    add work

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 "use strict";
-let age = 20;
+var HumanEnum;
+(function (HumanEnum) {
+    HumanEnum["M"] = "MALE";
+    HumanEnum["F"] = "FEMALE";
+})(HumanEnum || (HumanEnum = {}));
+const person = {
+    gender: HumanEnum.M
+};
+console.log(person.gender);
+const person1 = "shoaib";
+console.log(person1);
+let age = 23;
